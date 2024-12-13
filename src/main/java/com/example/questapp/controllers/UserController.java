e00e2bf55c13abf0cdb2a8d8d39de3e90dafa179
Zeliha İnan <zelihainann@gmail.com>
2024-12-13 23:33:35 +0300

    "entities" package, "repos" package, "controllers" package added

---
+++ src/main/java/com/example/questapp/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.example.questapp.controllers;
+
+import com.example.questapp.entities.User;
+import com.example.questapp.repos.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/questapp/repos/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.questapp.repos;
+
+import com.example.questapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
