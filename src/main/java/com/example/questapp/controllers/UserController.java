3e33c86e6539711320ecb4f76a8c586eb5d59a91
Zeliha İnan <zelihainann@gmail.com>
2024-12-14 13:40:13 +0300

    UserController added

--- src/main/java/com/example/questapp/controllers/UserController.java
+++ src/main/java/com/example/questapp/controllers/UserController.java
@@ -2,21 +2,50 @@ package com.example.questapp.controllers;
 
 import com.example.questapp.entities.User;
 import com.example.questapp.repos.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     private UserRepository userRepository;
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/{userId}")
+    public User getOneUser(@PathVariable Long userId) {
+        //custom exception
+        return userRepository.findById(userId).orElse(null);
+    }
+    
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId, @RequestBody User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            User foundUser = user.get();
+            foundUser.setUserName(newUser.getUserName());
+            foundUser.setPassword(newUser.getPassword());
+            userRepository.save(foundUser);
+            return foundUser;
+        }
+        return null;
+    }
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
     }
 }
--- src/main/java/com/example/questapp/entities/User.java
+++ src/main/java/com/example/questapp/entities/User.java
@@ -1,8 +1,6 @@
 package com.example.questapp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -10,7 +8,31 @@ import lombok.Data;
 @Data
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String userName;
     String password;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
