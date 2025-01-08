d5bd9acecda17fc35e5d4da702c7d51ff3f569e2
Zeliha İnan <zelihainann@gmail.com>
2025-01-08 21:41:55 +0300

    Bugfix avatar güncelleme

--- src/main/java/com/example/questapp/services/UserService.java
+++ src/main/java/com/example/questapp/services/UserService.java
@@ -43,8 +43,12 @@ public class UserService {
         Optional<User> user = userRepository.findById(userId);
         if (user.isPresent()) {
             User foundUser = user.get();
-            foundUser.setUserName(newUser.getUserName());
-            foundUser.setPassword(newUser.getPassword());
+            if (newUser.getUserName() != null) {
+                foundUser.setUserName(newUser.getUserName());
+            }
+            if (newUser.getPassword() != null) {
+                foundUser.setPassword(newUser.getPassword());
+            }
             foundUser.setAvatar(newUser.getAvatar());
             userRepository.save(foundUser);
             return foundUser;
