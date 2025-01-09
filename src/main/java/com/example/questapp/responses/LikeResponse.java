3332c804a1732c200268823e4074508c0107e1bf
Zeliha İnan <zelihainann@gmail.com>
2025-01-09 16:22:41 +0300

    Getter/Setter

--- src/main/java/com/example/questapp/responses/LikeResponse.java
+++ src/main/java/com/example/questapp/responses/LikeResponse.java
@@ -14,4 +14,28 @@ public class LikeResponse {
         this.userId = entity.getUser().getId();
         this.postId = entity.getPost().getId();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
 }
